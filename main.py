5b4b857e943680164201e3305d76485dc1503a81
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2022-11-24 05:06:46 +0000

    Correct directory name by removing preceding space

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+    return render_template("upload.html")
 
 
 @app.route('/', methods=['POST'])
